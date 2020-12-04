f221de0033bb2e43caf072ba0bdae0b14686f8c6
Richard <richard.wigley@gmail.com>
2020-12-04 13:07:46 +0000

    17 - font-awesome with webpacker (note: udemy example uses sprok)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,7 @@ import "channels"
 import "bootstrap/dist/js/bootstrap";
 import "bootstrap/dist/css/bootstrap";
 require("stylesheets/application.scss");
+import "@fortawesome/fontawesome-free/js/all";
 
 Rails.start()
 Turbolinks.start()
