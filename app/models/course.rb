d17c1c67d8ec8c8a274595de04541b9f125cfbf2
Richard <richard.wigley@gmail.com>
2020-12-05 22:57:28 +0000

    39-40 gem friendly_id - show user friendly links

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,9 @@
 class Course < ApplicationRecord
+  extend FriendlyId
   belongs_to :user
   has_rich_text :description
+  friendly_id :title, use: :slugged
+
   validates :title, presence: true
   validates :description, presence: true, length: { minimum: 5}
 end
