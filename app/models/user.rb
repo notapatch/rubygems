b8cd16a8bb9b3b6090943b18b72ab203d48c3f02
Richard <richard.wigley@gmail.com>
2020-12-06 10:40:00 +0000

    41-42 devise trackable - to keep user signing in information

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :courses
  
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :trackable
 
   def to_s
     email
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/index'
   devise_for :users
   resources :courses
+  resources :users, only: [:index]
   get 'home/index'
   root "home#index"
 end
