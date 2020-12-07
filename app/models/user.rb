450fbc914cc83c1b0bcca55a618822fb9fbdb858
Richard <richard.wigley@gmail.com>
2020-12-07 17:36:35 +0000

    51-52 gem ransack - level 1 - filter and search data in a table
      - added an order_by onto the ransack

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,10 @@ class User < ApplicationRecord
   def to_s
     email
   end
+
+  private
+
+  ransacker :sign_in_count do
+    Arel.sql("to_char(\"#{table_name}\".\"sign_in_count\", '99999')")
+  end
 end
