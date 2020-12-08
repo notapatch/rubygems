fdca22cedf15b1ab3af8f6a8ec7fddffb44e3982
Richard <richard.wigley@gmail.com>
2020-12-08 12:24:59 +0000

    55-56 gem ransack - level 3 - ransack search in navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  before_action :set_global_variables, if: :user_signed_in?
+
+  private
+
+  def set_global_variables
+    @ransack_courses = Course.ransack(params[:courses_search], search_key: :courses_search) #navbar search
+  end
 end
