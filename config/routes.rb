d3cfb8c21cb7aa3a009a72c266a905e49a51f564
Richard <richard.wigley@gmail.com>
2020-12-10 10:21:25 +0000

    64-65 gem rolify -edit user roles

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'users/index'
   devise_for :users
   resources :courses
-  resources :users, only: [:index]
+  resources :users, only: [:index, :edit, :update]
   get 'home/index'
   get 'home/activity'
   root "home#index"
